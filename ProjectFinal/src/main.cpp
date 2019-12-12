5f90e0b991f7c383f07ed674f2b5c64aca44a22d
Vortyy <etienne.dmenard@gmail.com>
2019-12-12 02:00:06 -0500

    Lecture et enregistrement des données dans des tableaux

--- ProjectFinal/src/main.cpp
+++ ProjectFinal/src/main.cpp
@@ -20,6 +20,9 @@
 
 using namespace std;
 
+void addLineToTables(string ligne, double tableQnt[9], string tableType[9], int index);
+double editQnt(string qnt, string tableType[9], int index);
+
 /* fonction: lancement du programme
  * param: int argc: nb paramètres recus de la ligne de commande
  * param: char ** argv: tableau contenant les parametres recus de ligne de commande
@@ -29,8 +32,7 @@ void main (int argc, char **argv) {
 	locale::global(locale{ "fr" });
 	string today, hier;
 	getDates(today, hier);
-	cout << today << endl;
-	cout << hier << endl;
+	cout << "Date d'aujourd'hui: " << today << endl << "Date d'hier: " << hier << endl << endl;
 
 	string pathInventaire = "./files/Inventaires_" + hier + ".txt";
 	string pathAchats = "./files/Achats_" + today + ".txt";
@@ -39,20 +41,70 @@ void main (int argc, char **argv) {
 	ifstream fichierInventaire(pathInventaire);
 	ifstream fichierAchats(pathAchats);
 	ifstream fichierVentes(pathVentes);
-	ofstream fichierAnalyse(pathAnalyse);
-	string ligne;
+	//ofstream fichierAnalyse(pathAnalyse); // not yet
+
+	string ligne; int index;
+	string tableNom[9] = { "Boeuf", "Pain" "Laitue", "Cheddar jaune", "Cheddar blanc", "Cornichons", "Oignons", "Tomate" "Sauce" };
+	string tableType[9];
+	double tableQntInv[9];
+	double tableQntAcht[9];
 
 	if (fichierInventaire) {
-		while (!fichierInventaire.eof())
-		{
+		index = 0;
+		while (!fichierInventaire.eof()) {
 			getline(fichierInventaire, ligne);
-			cout << ligne << endl;
+			addLineToTables(ligne, tableQntInv, tableNom, index);
+			index++;
 		}
 
 		fichierInventaire.close();
 	}
 	else cout << pathInventaire << " could not be opened." << endl;
 
+	if (fichierAchats) {
+		index = 0;
+		while (!fichierAchats.eof()) {
+			getline(fichierAchats, ligne);
+			addLineToTables(ligne, tableQntAcht, tableNom, index);
+			index++;
+		}
+
+		fichierAchats.close();
+	}
+	else cout << pathAchats << " could not be opened." << endl;
+
+	// stoi(string) => string to int
+	// stod(string) => string to double
+
 	cout << endl << "Programme par Étienne Ménard et Emily Bernier." << endl;
 	system("PAUSE");
+}
+
+void addLineToTables(string ligne, double tableQnt[9], string tableType[9], int index) {
+	string limiter = ";"; string temp;
+	auto start = 0U;
+	auto limit = ligne.find(limiter);
+
+	temp = ligne.substr(start, limit - start);
+
+	start = limit + limiter.length();
+	limit = ligne.find(limiter, start);
+	
+	string qnt = ligne.substr(start, limit - start);
+
+	start = limit + limiter.length();
+	limit = ligne.find(limiter, start);
+
+	tableType[index] = ligne.substr(start, limit - start);
+	tableQnt[index] = editQnt(qnt, tableType, index);
+}
+
+double editQnt(string qnt, string tableType[9], int index) {
+	double newQnt;
+	newQnt = stod(qnt);
+
+	if (tableType[index] == "kg") {
+		newQnt *= 1000;
+	}
+	return newQnt;
 }
