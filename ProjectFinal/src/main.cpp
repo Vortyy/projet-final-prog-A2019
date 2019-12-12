e7a9dd15703bcdb47c19f8ada436e94fd96f830b
Vortyy <etienne.dmenard@gmail.com>
2019-12-12 03:08:42 -0500

    Ingredients for sold food summed up

--- ProjectFinal/src/main.cpp
+++ ProjectFinal/src/main.cpp
@@ -20,8 +20,14 @@
 
 using namespace std;
 
-void addLineToTables(string ligne, double tableQnt[9], string tableType[9], int index);
-double editQnt(string qnt, string tableType[9], int index);
+void addLineToTables(string ligne, double tableQnt[], string tableType[], int index);
+double editQnt(string qnt, string tableType[], int index);
+void addIngQnt(string ligne, double tableQnt[], int index);
+void bigMac(double nbItem, double tableQntIng[]);
+void beeseChurger(double nbItem, double tableQntIng[]);
+void quartLivre(double nbItem, double tableQntIng[]);
+void doubleQuartLivre(double nbItem, double tableQntIng[]);
+void baconClub(double nbItem, double tableQntIng[]);
 
 /* fonction: lancement du programme
  * param: int argc: nb paramètres recus de la ligne de commande
@@ -48,6 +54,7 @@ void main (int argc, char **argv) {
 	string tableType[9];
 	double tableQntInv[9];
 	double tableQntAcht[9];
+	double tableQntIng[9] = { 0,0,0,0,0,0,0,0,0 };
 
 	if (fichierInventaire) {
 		index = 0;
@@ -73,14 +80,27 @@ void main (int argc, char **argv) {
 	}
 	else cout << pathAchats << " could not be opened." << endl;
 
-	// stoi(string) => string to int
-	// stod(string) => string to double
+	if (fichierVentes) {
+		index = 0;
+		while (!fichierVentes.eof()) {
+			getline(fichierVentes, ligne);
+			addIngQnt(ligne, tableQntIng, index);
+			index++;
+		}
+
+		for (int i = 0; i < 9; i++) {
+			cout << tableQntIng[i] << endl;
+		}
+		fichierVentes.close();
+	}
+	else cout << pathVentes << " could not be opened." << endl;
+
 
 	cout << endl << "Programme par Étienne Ménard et Emily Bernier." << endl;
 	system("PAUSE");
 }
 
-void addLineToTables(string ligne, double tableQnt[9], string tableType[9], int index) {
+void addLineToTables(string ligne, double tableQnt[], string tableType[], int index) {
 	string limiter = ";"; string temp;
 	auto start = 0U;
 	auto limit = ligne.find(limiter);
@@ -107,4 +127,90 @@ double editQnt(string qnt, string tableType[9], int index) {
 		newQnt *= 1000;
 	}
 	return newQnt;
+}
+
+void addIngQnt(string ligne, double tableQnt[], int index) {
+	string limiter = ";"; string temp;
+	double ventes; double nbItem;
+	auto start = 0U; auto limit = ligne.find(limiter);
+
+	temp = ligne.substr(start, limit - start);
+	start = limit + limiter.length();
+	limit = ligne.find(limiter, start);
+
+	string qnt = ligne.substr(start, limit - start);
+	ventes = stoi(qnt);
+	
+	switch (index) {
+	case 0: // big mac
+		nbItem = ceil(ventes / 5.23);
+		bigMac(nbItem, tableQnt);
+		break;
+
+	case 1:
+		nbItem = ceil(ventes / 2.62);
+		beeseChurger(nbItem, tableQnt);
+		break;
+
+	case 2:
+		nbItem = ceil(ventes / 4.96);
+		quartLivre(nbItem, tableQnt);
+		break;
+
+	case 3:
+		nbItem = ceil(ventes / 6.27);
+		doubleQuartLivre(nbItem, tableQnt);
+		break;
+
+	case 4:
+		nbItem = ceil(ventes / 5.88);
+		baconClub(nbItem, tableQnt);
+	default:
+		break;
+	}
+}
+
+void bigMac(double nbItem, double tableQntIng[]) {
+	tableQntIng[0] += nbItem * 2 * 45.4;
+	tableQntIng[1] += nbItem * 3;
+	tableQntIng[2] += nbItem * 2;
+	tableQntIng[3] += nbItem * 1;
+	tableQntIng[5] += nbItem * 3;
+	tableQntIng[6] += nbItem * 12;
+	tableQntIng[8] += nbItem * 7;
+}
+
+void beeseChurger(double nbItem, double tableQntIng[]) {
+	tableQntIng[0] += nbItem * 45.4;
+	tableQntIng[1] += nbItem * 2;
+	tableQntIng[3] += nbItem * 1;
+	tableQntIng[5] += nbItem * 2;
+	tableQntIng[6] += nbItem * 6;
+}
+
+void quartLivre(double nbItem, double tableQntIng[]) {
+	tableQntIng[0] += nbItem * 113;
+	tableQntIng[1] += nbItem * 2;
+	tableQntIng[3] += nbItem * 1;
+	tableQntIng[5] += nbItem * 3;
+	tableQntIng[6] += nbItem * 12;
+}
+
+void doubleQuartLivre(double nbItem, double tableQntIng[]) {
+	tableQntIng[0] += nbItem * 226;
+	tableQntIng[1] += nbItem * 2;
+	tableQntIng[3] += nbItem * 2;
+	tableQntIng[5] += nbItem * 4;
+	tableQntIng[6] += nbItem * 12;
+}
+
+void baconClub(double nbItem, double tableQntIng[]) {
+	tableQntIng[0] += nbItem * 113;
+	tableQntIng[1] += nbItem * 2;
+	tableQntIng[2] += nbItem * 3;
+	tableQntIng[4] += nbItem * 1;
+	tableQntIng[5] += nbItem * 4;
+	tableQntIng[7] += nbItem * 2;
+	tableQntIng[8] += nbItem * 7;
+
 }
